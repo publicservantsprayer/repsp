992c2b53613ee7dd540b7a38d04fb847e235fef3
Anna Amick <anna.amick@gmail.com>
2019-09-24 15:51:58 -0400

    updated events and news

--- src/screens/News.js
+++ src/screens/News.js
@@ -8,29 +8,10 @@ import { withFirebase } from '../Firebase'
 
 const db = [
   {
-    title: 'About Us',
-    image:
-      'https://images.squarespace-cdn.com/content/v1/55a52fd4e4b037b6dd04e384/1516849619525-SIMH5C28K2UA4M77R4A7/ke17ZwdGBToddI8pDm48kIw2UfM4pFw-uQov_WM0sxJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UQenKorOgdPvqNz1rrSY2twX-9Ss2mhmBeQBYMjFx-LvtNpR-oeFwj2Ae_lbqFbpxA/3.jpg?format=750w',
-    blurb: 'Find out our mission statement, what we do, and meet the Team!',
-  },
-  {
-    title: 'PSP Newsletters',
-    image:
-      'https://alphaministries.com/wp-content/uploads/2019/04/IMG_0285.jpeg',
-    blurb: "Learn the current news on what's going on in PSP",
-  },
-  {
     title: 'Live Prayer Requests From Leaders',
     image: 'https://saintmike.com/wp-content/uploads/MilitaryPrayerList1.jpg',
     blurb:
       'Want to pray more specifically? Here are live prayer requests from our leaders.',
-  },
-  {
-    title: 'Changing the tone of politics through prayer',
-    image:
-      'https://s3-ap-southeast-2.amazonaws.com/nz.dkhosting.com.au/wp-content/uploads/2017/06/22105108/B17__If-my-people-pray-1024x576.jpg',
-    blurb:
-      'Hear encouraging testimonies and find out what others are saying about PSP.',
   },
   {
     title: 'Pray, Give, Volunteer',
