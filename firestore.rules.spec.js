5447ed462f6400fd513d07a84eff975ea101e673
Nathan Amick <github@nathanamick.com>
2019-10-07 11:22:49 -0400

    Update firestore rules

--- firestore.rules.spec.js
+++ firestore.rules.spec.js
@@ -1,20 +1,60 @@
 const { setup, teardown } = require('./spec/helpers')
 
-describe('Database rules', () => {
+
+describe('Unathenticated rules', () => {
   let db
-  let ref
 
-  beforeAll(async () => {
-    db = await setup()
-    ref = db.collection('nonexistent-collection')
-  })
+  beforeAll(async () => db = await setup())
 
-  afterAll(async () => {
-    await teardown()
-  })
+  afterAll(async () => await teardown())
 
   test('fail when reading/writing to an unauthorized collection', async () => {
+    const ref = db.collection('nonexistent-collection')
+
+    await expect(ref.get()).toDeny()
+    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+  })
+
+  test('fail when reading/writing to adminUsers', async () => {
+    const ref = db.collection('adminUsers')
+
+    await expect(ref.get()).toDeny()
+    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+  })
+
+  test('read-only from states', async () => {
+    const ref = db.collection('states')
+
     await expect(ref.get()).toAllow()
+    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+  })
+
+  test('read-only from content', async () => {
+    const ref = db.collection('content')
+
+    await expect(ref.get()).toAllow()
+    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+  })
+})
+
+describe('Authenticated rules', () => {
+  const data = {
+    'adminUsers/admin123': { 'uid': 'admin123' }
+  }
+
+  afterAll(async () => await teardown())
+
+  test('write allowed from admin', async () => {
+    db = await setup({ uid: 'admin123' }, data)
+    const ref = db.collection('content')
+
+    await expect(ref.doc('test').set({ foo: 'bar' })).toAllow()
+  })
+
+  test('write denied from non admin', async () => {
+    db = await setup({ uid: 'joe123' }, data)
+    const ref = db.collection('content')
+
     await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
   })
 })
