0ae32557fc80c4d771701cd7e195cdf2c52ef3b1
Nathan Amick <github@nathanamick.com>
2019-10-07 12:48:22 -0400

    Add getting adminUser to firestore rules

--- firestore.rules.spec.js
+++ firestore.rules.spec.js
@@ -1,60 +1,91 @@
 const { setup, teardown } = require('./spec/helpers')
 
-
-describe('Unathenticated rules', () => {
-  let db
-
-  beforeAll(async () => db = await setup())
+describe(' Database rules', () => {
 
   afterAll(async () => await teardown())
 
-  test('fail when reading/writing to an unauthorized collection', async () => {
-    const ref = db.collection('nonexistent-collection')
+  describe('non-existent collection', () => {
 
-    await expect(ref.get()).toDeny()
-    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+    test('reading/writing denied from all', async () => {
+      const db = await setup()
+      const ref = db.collection('nonexistent-collection')
+
+      await expect(ref.get()).toDeny()
+      await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+    })
   })
 
-  test('fail when reading/writing to adminUsers', async () => {
-    const ref = db.collection('adminUsers')
+  describe('content collection', () => {
+    test('read-only from all', async () => {
+      const db = await setup()
+      const ref = db.collection('content')
 
-    await expect(ref.get()).toDeny()
-    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+      await expect(ref.get()).toAllow()
+      await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+    })
+
+    describe('authenticated', () => {
+      const data = {
+        'adminUsers/admin123': { 'uid': 'admin123' }
+      }
+
+      test('write allowed from admin', async () => {
+        db = await setup({ uid: 'admin123' }, data)
+        const ref = db.collection('content')
+
+        await expect(ref.doc('test').set({ foo: 'bar' })).toAllow()
+      })
+
+      test('write denied from non admin', async () => {
+        db = await setup({ uid: 'joe123' }, data)
+        const ref = db.collection('content')
+
+        await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+      })
+    })
   })
 
-  test('read-only from states', async () => {
-    const ref = db.collection('states')
+  describe('states collection', () => {
+    test('read-only from all', async () => {
+      const db = await setup()
+      const ref = db.collection('states')
 
-    await expect(ref.get()).toAllow()
-    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+      await expect(ref.get()).toAllow()
+      await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+    })
   })
 
-  test('read-only from content', async () => {
-    const ref = db.collection('content')
+  describe('adminUsers collection', () => {
+    const data = {
+      'adminUsers/admin123': { 'uid': 'admin123' }
+    }
 
-    await expect(ref.get()).toAllow()
-    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
-  })
-})
+    test('get denied from non-authenticated user', async () => {
+      db = await setup(null, data)
+      const ref = db.collection('adminUsers')
 
-describe('Authenticated rules', () => {
-  const data = {
-    'adminUsers/admin123': { 'uid': 'admin123' }
-  }
+      await expect(ref.doc('joe123').get()).toDeny()
+    })
 
-  afterAll(async () => await teardown())
+    test('get allowed from authenticated user', async () => {
+      db = await setup({ uid: 'joe123' }, data)
+      const ref = db.collection('adminUsers')
 
-  test('write allowed from admin', async () => {
-    db = await setup({ uid: 'admin123' }, data)
-    const ref = db.collection('content')
+      await expect(ref.doc('joe123').get()).toAllow()
+    })
 
-    await expect(ref.doc('test').set({ foo: 'bar' })).toAllow()
-  })
+    test('list denied to all', async () => {
+      db = await setup({ uid: 'joe123' }, data)
+      const ref = db.collection('adminUsers')
 
-  test('write denied from non admin', async () => {
-    db = await setup({ uid: 'joe123' }, data)
-    const ref = db.collection('content')
+      await expect(ref.get()).toDeny()
+    })
 
-    await expect(ref.doc('test').set({ foo: 'bar' })).toDeny()
+    test('write denied to all', async () => {
+      db = await setup({ uid: 'joe123' }, data)
+      const ref = db.collection('adminUsers')
+
+      await expect(ref.doc('joe123').set({ foo: 'bar' })).toDeny()
+    })
   })
 })
