078527c19d1210d173a959d3f72f1ff7b8f05c95
Anna Amick <anna.amick@gmail.com>
2019-10-11 20:43:14 -0400

    update markdown

--- src/Markdown.js
+++ src/Markdown.js
@@ -3,6 +3,7 @@ import ReactMarkdown from 'markdown-to-jsx'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
+import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
   listItem: {
@@ -14,26 +15,38 @@ const Image = props => {
   return <img style={{ maxWidth: '100%', height: 'auto' }} alt="" {...props} />
 }
 
+const LinkButton = ({ children, ...restOfProps }) => {
+  return (
+    <Button variant="contained" {...restOfProps}>
+      {children}
+    </Button>
+  )
+}
+
 const options = {
   overrides: {
     h1: {
       component: Typography,
       props: {
         gutterBottom: true,
-        variant: 'h4',
+        variant: 'h1',
       },
     },
-    h2: { component: Typography, props: { gutterBottom: true, variant: 'h6' } },
+    h2: { component: Typography, props: { gutterBottom: true, variant: 'h2' } },
     h3: {
       component: Typography,
-      props: { gutterBottom: true, variant: 'subtitle1' },
+      props: { gutterBottom: true, variant: 'h3' },
     },
     h4: {
       component: Typography,
-      props: { gutterBottom: true, variant: 'caption', paragraph: true },
+      props: { gutterBottom: true, variant: 'h4', paragraph: true },
     },
     p: { component: Typography, props: { paragraph: true, component: 'div' } },
-    a: { component: Link },
+
+    a: {
+      component: Link,
+      props: { component: LinkButton, color: 'secondary' },
+    },
     img: {
       component: Image,
     },
@@ -47,6 +60,6 @@ const options = {
   },
 }
 
-export default function Markdown (props) {
+export default function Markdown(props) {
   return <ReactMarkdown options={options} {...props} />
 }
