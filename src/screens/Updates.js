ab244f2794f03ee7866eda6cede31bd3afb59eb2
Nathan Amick <github@nathanamick.com>
2019-10-28 13:55:05 -0400

    Fix Updates screen

--- src/screens/Updates.js
+++ src/screens/Updates.js
@@ -30,39 +30,37 @@ const ArticleGrid = ({ article }) => {
   if (error) console.log('Error loading image: ', error)
 
   return (
-    <Screen>
-      <Box flexGrow={1} bgcolor="common.white" px={3} py={1}>
-        <Grid item sm={4}>
-          <Paper className={classes.root}>
-            <Box
-              maxWidth={375}
-              height="auto"
-              mx={2}
-              my={2}
-              px={1}
-              py={2}
-              boxShadow={9}
-              bgcolor="common.white"
-              color="common.black"
-              flexGrow={1}
-            >
-              <Box>
-                <h1>
-                  {moment(article.createdOn.toDate()).format('MMMM Do, YYYY')}
-                </h1>
-              </Box>
-              <Box>
-                {loading && <p>Loading...</p>}
-                {image && src && (
-                  <img style={{ width: '100%' }} src={src} alt="" />
-                )}
-              </Box>
-              <Box>{article.content}</Box>
+    <Box flexGrow={1} bgcolor="common.white" px={3} py={1}>
+      <Grid item sm={4}>
+        <Paper className={classes.root}>
+          <Box
+            maxWidth={375}
+            height="auto"
+            mx={2}
+            my={2}
+            px={1}
+            py={2}
+            boxShadow={9}
+            bgcolor="common.white"
+            color="common.black"
+            flexGrow={1}
+          >
+            <Box>
+              <h1>
+                {moment(article.createdOn.toDate()).format('MMMM Do, YYYY')}
+              </h1>
             </Box>
-          </Paper>
-        </Grid>
-      </Box>
-    </Screen>
+            <Box>
+              {loading && <p>Loading...</p>}
+              {image && src && (
+                <img style={{ width: '100%' }} src={src} alt="" />
+              )}
+            </Box>
+            <Box>{article.content}</Box>
+          </Box>
+        </Paper>
+      </Grid>
+    </Box>
   )
 }
 
@@ -72,18 +70,20 @@ export default () => {
   if (docs) console.log(docs)
 
   return (
-    <Container maxWidth="lg">
-      <Grid
-        container
-        direction="row"
-        justify="space-evenly"
-        alignItems="center"
-        spacing={10}
-      >
-        {loading && <p>Loading...</p>}
-        {docs &&
-          docs.map((article, i) => <ArticleGrid article={article} key={i} />)}
-      </Grid>
-    </Container>
+    <Screen>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+          spacing={10}
+        >
+          {loading && <p>Loading...</p>}
+          {docs &&
+            docs.map((article, i) => <ArticleGrid article={article} key={i} />)}
+        </Grid>
+      </Container>
+    </Screen>
   )
 }
