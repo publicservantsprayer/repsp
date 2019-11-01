ac62e6f6a9ce224c040aeb7ff517a92c479d7dcc
Nathan Amick <github@nathanamick.com>
2019-11-01 16:57:59 -0400

    Detect Facebook width

--- src/FacebookTimeline.js
+++ src/FacebookTimeline.js
@@ -1,30 +1,38 @@
 import React, { useEffect } from 'react'
 import Box from '@material-ui/core/Box'
+import { withSize } from 'react-sizeme'
 
 import useUSAState from './utilities/useUSAState'
 
-export default function FacebookTimeline({ accountName }) {
+export default withSize()(function FacebookTimeline({ size }) {
+  const [width, setWidth] = React.useState()
   const { facebookPage } = useUSAState()
+  const facebook = window.FB
 
   useEffect(() => {
-    window.FB.init({
-      appId: '2421676531429086',
-      version: 'v4.0',
-      xfbml: true,
-    })
-  }, [])
+    setWidth(size.width)
+    if (facebook && width > 0) {
+      facebook.init({
+        appId: '2421676531429086',
+        version: 'v4.0',
+        xfbml: true,
+      })
+    }
+  }, [facebook, width, size.width])
 
   return (
-    <Box
-      className="fb-page"
-      data-href={`https://www.facebook.com/${facebookPage}/`}
-      data-tabs="timeline"
-      data-width="340"
-      data-height=""
-      data-small-header="true"
-      data-adapt-container-width="true"
-      data-hide-cover="true"
-      data-show-facepile="false"
-    />
+    <Box>
+      <Box
+        className="fb-page"
+        data-href={`https://www.facebook.com/${facebookPage}/`}
+        data-tabs="timeline"
+        data-width={size.width}
+        data-height=""
+        data-small-header="true"
+        data-adapt-container-width="true"
+        data-hide-cover="true"
+        data-show-facepile="false"
+      />
+    </Box>
   )
-}
+})
