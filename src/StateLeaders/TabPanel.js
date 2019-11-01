039a9ba448d8197037069cbea6e9a3a129ada49a
Nathan Amick <github@nathanamick.com>
2019-11-01 13:18:38 -0400

    Center leaders on mobile

--- src/StateLeaders/TabPanel.js
+++ src/StateLeaders/TabPanel.js
@@ -1,35 +1,36 @@
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import { H4 } from '../utilities/formating'
 import useUSAState from '../utilities/useUSAState'
 import Leaders from './Leaders'
+import useMobile from '../utilities/useMobile'
+import { Divider } from '@material-ui/core'
 
 export default function TabPanel({ chamber, currentTab, index }) {
   const { stateName } = useUSAState()
   const chamberTitle = chamber === 'H' ? 'Representatives' : 'Senators'
+  const mobile = useMobile()
+  const align = mobile ? 'center' : 'left'
 
   return (
-    <Box display="flex" justifyContent="center">
-      <Typography component="div" role="tabpanel" hidden={currentTab !== index}>
-        <Box px={2} pt={5}>
-          <H4>
-            US {chamberTitle} from {stateName}
-          </H4>
-        </Box>
-        <Box display="flex" flexWrap="wrap" justifyContent="left">
-          <Leaders legType="FL" chamber={chamber} />
-        </Box>
-
-        <Box px={2} pt={5}>
-          <H4>
-            {stateName} State {chamberTitle}
-          </H4>
-        </Box>
-        <Box display="flex" flexWrap="wrap" justifyContent="left">
-          <Leaders legType="SL" chamber={chamber} />
-        </Box>
-      </Typography>
+    <Box role="tabpanel" hidden={currentTab !== index}>
+      <Box px={2} pt={5} textAlign={align}>
+        <H4>
+          US {chamberTitle} from {stateName}
+        </H4>
+      </Box>
+      <Box display="flex" flexWrap="wrap" justifyContent={align} mb={3}>
+        <Leaders legType="FL" chamber={chamber} />
+      </Box>
+      <Divider />
+      <Box px={2} pt={5} textAlign={align}>
+        <H4>
+          {stateName} State {chamberTitle}
+        </H4>
+      </Box>
+      <Box display="flex" flexWrap="wrap" justifyContent={align}>
+        <Leaders legType="SL" chamber={chamber} />
+      </Box>
     </Box>
   )
 }
