b749cd6ec0a93ca2bab99fd4696da707b16030d6
aamick <anna.amick@gmail.com>
2021-01-19 07:38:00 -0500

    Add script to normalize data

--- src/Data/DataTable.js
+++ src/Data/DataTable.js
@@ -12,46 +12,54 @@ import moment from 'moment'
 const dateID = moment().format('YYYY-MM-DD')
 const previousDayID = dateID => moment(dateID).subtract(1, 'day').format('YYYY-MM-DD')
 
+const formatTimestamp = timestamp => {
+  if (!timestamp) return 'none'
+  return moment.unix(timestamp.seconds).format('YYYY-MM-DD')
+}
+
 const useData = () => {
   const stateCode = 'IN'
   const { db } = useFirebase()
+  const [rows, setRows] = React.useState([])
 
-  const leadersRef = db
-    .collection('states')
-    .doc(stateCode)
-    .collection('leaders')
-    .where('lastImportDate', '>', new Date('2020-08-15'))
-    .where('hasPhoto', '==', true)
-    .orderBy('lastImportDate')
-    .orderBy('LastName')
-    .orderBy('PID')
-  const postsRef = db.collection('states').doc(stateCode).collection('posts')
+  const leadersRef = db.collection('states').doc(stateCode).collection('leaders')
+  //.where('lastImportDate', '==', null)
+  // .where('lastImportDate', '>', new Date('2010-08-15'))
+  //.where('hasPhoto', '==', true)
+  //.orderBy('lastImportDate')
+  //.orderBy('LastName')
+  //.orderBy('PID')
+  // const postsRef = db.collection('states').doc(stateCode).collection('posts')
 
   const handleGetData = async () => {
-    const previousPost = await postsRef.doc(previousDayID(dateID)).get()
-    console.log(previousPost)
+    console.log('getting leaders')
+    const leaders = await leadersRef.get()
+    // console.log(leaders)
+    setRows(leaders.docs.map(d => d.data()))
+    //   const previousPost = await postsRef.doc(previousDayID(dateID)).get()
+    //   console.log(previousPost)
 
-    const firstThree = await leadersRef.limit(3).get()
-    console.log(firstThree)
+    //   const firstThree = await leadersRef.limit(3).get()
+    //   console.log(firstThree)
 
-    let docs = firstThree.docs
+    //   let docs = firstThree.docs
 
-    if (previousPost.exists) {
-      const lastLeader = previousPost.data().leader3
-      console.log({ lastLeader })
-      const nextThree = await leadersRef
-        .startAfter(lastLeader.LastName, lastLeader.PID)
-        .limit(3)
-        .get()
-      console.log({ nextThree })
-      // Add on the first three in case we need to wrap
-      docs = nextThree.docs.concat(docs)
-    }
+    //   if (previousPost.exists) {
+    //     const lastLeader = previousPost.data().leader3
+    //     console.log({ lastLeader })
+    //     const nextThree = await leadersRef
+    //       .startAfter(lastLeader.LastName, lastLeader.PID)
+    //       .limit(3)
+    //       .get()
+    //     console.log({ nextThree })
+    //     // Add on the first three in case we need to wrap
+    //     docs = nextThree.docs.concat(docs)
+    //   }
 
-    console.log({ dateID, docs, postsRef })
+    //   console.log({ dateID, docs, postsRef })
   }
 
-  return { rows: [], handleGetData }
+  return { rows, handleGetData }
 }
 
 function DataTable() {
@@ -65,23 +73,21 @@ function DataTable() {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>lastImportDate</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">link</TableCell>
+              <TableCell align="right">has photo</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map(row => (
-              <TableRow key={row.name}>
+              <TableRow key={row.PID}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {formatTimestamp(row.lastImportDate)}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.LegalName}</TableCell>
+                <TableCell align="right">{row.permaLink}</TableCell>
+                <TableCell align="right">{row.hasPhoto ? 'yes' : 'no'}</TableCell>
               </TableRow>
             ))}
           </TableBody>
