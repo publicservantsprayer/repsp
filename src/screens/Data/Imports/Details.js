bedb924af33c09f94a33c3678a409a9733e96bb1
Nathan Amick <nathan.amick@lifeomic.com>
2021-03-08 08:20:41 -0500

    Improve logs

--- src/screens/Data/Imports/Details.js
+++ src/screens/Data/Imports/Details.js
@@ -25,51 +25,17 @@ const useDataImport = dataImportId => {
 const useImportLogs = dataImportId => {
   const { db } = useFirebase()
 
-  const [stateImportLogs, stateLoading, stateError] = useCollectionData(
-    db
-      .collection('/dataImports')
-      .doc(dataImportId)
-      .collection('importLog')
-      .where('legislatorType', '==', 'state')
-      .orderBy('timestamp'),
+  const [importLogs] = useCollectionData(
+    db.collection('/dataImports').doc(dataImportId).collection('importLog').orderBy('timestamp'),
     {
       idField: 'docId',
     }
   )
-
-  const [federalImportLogs, federalLoading, federalError] = useCollectionData(
-    db
-      .collection('/dataImports')
-      .doc(dataImportId)
-      .collection('importLog')
-      .where('legislatorType', '==', 'federal')
-      .orderBy('timestamp'),
-    {
-      idField: 'docId',
-    }
-  )
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const snapshot = await db
-        .collection('/dataImports')
-        .doc(dataImportId)
-        .collection('importLog')
-        .where('legislatorType', '==', 'federal')
-        .orderBy('timestamp')
-        .get()
-      console.log(snapshot)
-    }
-    fetchData()
-  }, [dataImportId, db])
 
   return {
-    stateImportLogs,
-    federalImportLogs,
-    stateLoading,
-    stateError,
-    federalLoading,
-    federalError,
+    stateImportLogs: importLogs?.filter(log => log.legislatorType === 'state'),
+    federalImportLogs: importLogs?.filter(log => log.legislatorType === 'federal'),
+    importLogs: importLogs?.filter(log => log.legislatorType === undefined),
   }
 }
 
@@ -77,7 +43,7 @@ export default function DataImportDetails() {
   const [importStarted] = React.useState(false)
   const { dataImportId } = useParams()
   const [dataImport] = useDataImport(dataImportId)
-  const { stateImportLogs, federalImportLogs } = useImportLogs(dataImportId)
+  const { stateImportLogs, federalImportLogs, importLogs } = useImportLogs(dataImportId)
 
   if (!dataImport)
     return (
@@ -89,17 +55,30 @@ export default function DataImportDetails() {
   return (
     <Layout>
       <Box margin={2}>
-        <h1>
-          Data Import from{' '}
-          {moment.unix(dataImport?.date?.seconds).format('dddd, MMMM Do [at] h:mm:ss a')}
-        </h1>
-        <Box>
-          <h2>Federal</h2>
-          <Link href={dataImport.federalMembersUrl}>{dataImport.federalMembersUrl}</Link>
-        </Box>
-        <Box>
-          <h2>State</h2>
-          <Link href={dataImport.stateMembersUrl}>{dataImport.stateMembersUrl}</Link>
+        <Box display="flex" justifyContent="space-between" width="100%">
+          <Box width="49%">
+            <h1>
+              Data Import from{' '}
+              {moment.unix(dataImport?.date?.seconds).format('dddd, MMMM Do [at] h:mm:ss a')}
+            </h1>
+            <Box>
+              <h2>Federal</h2>
+              <Link href={dataImport.federalMembersUrl}>{dataImport.federalMembersUrl}</Link>
+            </Box>
+            <Box>
+              <h2>State</h2>
+              <Link href={dataImport.stateMembersUrl}>{dataImport.stateMembersUrl}</Link>
+            </Box>
+          </Box>
+          <Box width="49%">
+            <h2> Import logs:</h2>
+            <Box height={400} border="1px solid white" overflow="auto" display="block">
+              <ul>
+                {importStarted && <li>Import started...</li>}
+                {importLogs && importLogs.reverse().map((log, i) => <li key={i}>{log.message}</li>)}
+              </ul>
+            </Box>
+          </Box>
         </Box>
         <Box display="flex" justifyContent="space-between" width="100%">
           <Box width="49%">
@@ -108,11 +87,7 @@ export default function DataImportDetails() {
               <ul>
                 {importStarted && <li>Import started...</li>}
                 {stateImportLogs &&
-                  stateImportLogs.reverse().map((log, i) => (
-                    <li key={i}>
-                      {log.legislatorType}: {log.message}
-                    </li>
-                  ))}
+                  stateImportLogs.reverse().map((log, i) => <li key={i}>{log.message}</li>)}
               </ul>
             </Box>
           </Box>
@@ -122,11 +97,7 @@ export default function DataImportDetails() {
               <ul>
                 {importStarted && <li>Import started...</li>}
                 {federalImportLogs &&
-                  federalImportLogs.reverse().map((log, i) => (
-                    <li key={i}>
-                      {log.legislatorType}: {log.message}
-                    </li>
-                  ))}
+                  federalImportLogs.reverse().map((log, i) => <li key={i}>{log.message}</li>)}
               </ul>
             </Box>
           </Box>
